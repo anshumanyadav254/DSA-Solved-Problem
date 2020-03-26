dad21f9ca850ae998d697a787f80cbb10eadf91e
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-03-26 14:19:44 +0530

    Create jonephus_problem.java

---
+++ jonephus_problem.java
@@ -0,0 +1,26 @@
+import java.util.
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		System.out.println(fun(n,k));
+
+	}
+	static int fun(int n,int k)
+	{
+	    if(n==1)
+	    {
+	        return 0;
+	    }
+	    else
+	    {
+	        return (fun(n-1,k)+k)%n;
+	        
+	    }
+
+	}
+}
