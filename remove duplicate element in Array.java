deed29d820eadc1cc08225b6c4689960b55fc63a
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-03-29 10:22:28 +0530

    Create remove duplicate element in Array.java

---
+++ remove duplicate element in Array.java
@@ -0,0 +1,14 @@
+static int fun(int arr[],int n){
+	    int temp[]=new int[n];
+	    temp[0]=arr[0];
+	    int res=1;
+	    for(int i=1;i<n;i++){
+	        if(temp[res-1]!=arr[i]){
+	            temp[res]=arr[i];
+	            res++;
+	        }
+	    }
+	    for(int i=0;i<res;i++){
+	        arr[i]=temp[i];
+	    }
+	    return res;
