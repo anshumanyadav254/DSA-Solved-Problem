dc669c727a54a254122834b6528071448b5347c2
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-04-05 16:27:44 +0530

    Create Equilibrium Point (index) in array.java

---
+++ Equilibrium Point (index) in array.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n =sc.nextInt();
+        int arr []=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(Eq(arr,n));
+    }
+    static int Eq(int arr[],int n)
+    {
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+arr[i];
+        }
+        int l_sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(l_sum==sum-arr[i])
+            {
+                return i+1;
+            }
+            l_sum=l_sum+arr[i];
+            sum=sum-arr[i];
+        }
+        return -1;
+
+    }
+}
