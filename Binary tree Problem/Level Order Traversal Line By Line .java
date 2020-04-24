421855f49b218914092c3e4f7f074628c2ecd9cd
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-04-24 13:02:43 +0530

    Create Level Order Traversal Line By Line .java

---
+++ Binary tree Problem/Level Order Traversal Line By Line .java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node{
+	int data;
+	Node left;
+	Node right;
+	Node(int k)
+	{
+		data=k;
+	}
+}
+public class PrintLevelLine
+{
+	public static void main(String args[]) {
+		Node root = new Node(10);
+		root.left = new Node(20);
+		root.right = new Node(30);
+		root.left.left = new Node(40);
+		root.left.right = new Node(32);
+		root.right.left = new Node(50);
+		root.right.left.left = new Node(70);
+		LevelTravesalLine(root);
+		System.out.println();
+	}
+	static  void LevelTravesalLine(Node root)
+	{
+		if(root==null)
+		{
+			return;
+		}
+
+		Queue<Node> q=new LinkedList<Node>();
+		q.add(root);
+		while(q.isEmpty()==false)
+		{
+			int count=q.size();
+			for(int i=0;i<count;i++)
+			{
+				Node curr =q.poll();
+				System.out.print(curr.data+" ");
+				if(curr.left!=null)
+				{
+					q.add(curr.left);
+				}
+				if(curr.right!=null)
+				{
+					q.add(curr.right);
+				}
+
+			}
+			System.out.println();
+
+		}
+}
+}
+// OutPut=10 
+// 20 30 
+// 40 32 50 
+// 70
