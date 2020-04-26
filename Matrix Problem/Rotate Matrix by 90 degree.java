921205e53f36affd32cfe891b7ef7e628266e5d7
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-04-26 17:42:47 +0530

    Create Rotate Matrix by 90 degree.java

---
+++ Matrix Problem/Rotate Matrix by 90 degree.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+        int  t= sc.nextInt();
+        while(t-->0)
+        {
+            int r=sc.nextInt();
+            //int c=sc.nextInt();
+            int Mat[][]=new int [r][r];
+            for(int i=0;i<r;i++)
+            {
+                for(int j=0;j<r;j++)
+                {
+                    Mat[i][j]=sc.nextInt();
+
+                }
+            }
+            transpose(Mat,r);
+            ReverseCol(Mat,r);
+            for(int i=0;i<r;i++)
+            {
+                for(int j=0;j<r;j++){
+                    System.out.print(Mat[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+        
+	}
+	static void transpose(int arr[][],int n)
+	{
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=i+1;j<n;j++)
+	        {
+	            int temp=arr[i][j];
+	            arr[i][j]=arr[j][i];
+	            arr[j][i]=temp;
+	        }
+	    }
+	}
+	static void ReverseCol(int arr[][],int n)
+	{
+	    for (int i=0;i<n;i++)
+	    {
+	        int low=0,high=n-1;
+	        while(low<high)
+	        {
+	            int temp=arr[low][i];
+	            arr[low][i]=arr[high][i];
+	            arr[high][i]=temp;
+	            low++;
+	            high--;
+	        }
+	    }
+	}
+}
+// O(n*n) space=O(1)
