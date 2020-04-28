0bb78ad8cd6f23268e16a6b1ea6c4f8dba833674
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-04-29 01:45:16 +0530

    Create Print left element of any tree.java

---
+++ Binary tree Problem/Print left element of any tree.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node{
+	int data;
+	Node left;
+	Node right;
+	Node(int k)
+	{
+		data=k;
+	}
+}
+
+public class PrintLevelLine
+{
+	public static void main(String args[]) {
+		Node root = new Node(10);
+		root.left = new Node(20);
+		root.right = new Node(30);
+		root.left.left = new Node(40);
+		root.left.right = new Node(32);
+		root.right.left = new Node(50);
+		root.right.left.left = new Node(70);
+		//LevelTravesalLine(root);
+		PrintLeftView(root);
+		System.out.println();
+	}
+static int maxlevel=0;
+	static  void printLeft(Node root,int level)
+	{
+		if(root==null)
+		{
+			return ;
+		}
+		if(maxlevel<level)
+		{
+			System.out.print(root.data+" ");
+			maxlevel=level;
+		}
+		printLeft(root.left,level+1);
+		printLeft(root.right,level+1);
+	}
+	static void PrintLeftView(Node root){
+		printLeft(root,1);
+	}
+ }
+ 
+ 
+ // out put =10 20 40 70 
