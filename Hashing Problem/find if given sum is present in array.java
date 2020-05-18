3786956734e9ec3a1392b6e9782810e527fe047f
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-05-18 16:44:57 +0530

    Create find if given sum is present in array.java

---
+++ Hashing Problem/find if given sum is present in array.java
@@ -0,0 +1,58 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class ZeroSumSubArray {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+
+		}
+		int sum=sc.nextInt();
+		System.out.println(isSumInSubArrayNaiv(arr,n,sum));
+		//System.out.println(IsSumSubarray(arr,n,sum));
+	}
+	static boolean isSumInSubArrayNaiv(int arr[],int n,int sum)
+	{
+		int curr_sum=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i;j<n;j++)
+			{
+				curr_sum+=arr[i];
+				if(curr_sum==sum)
+				{
+					return  true;
+				}
+			}
+
+
+		}
+		return false;
+	}
+	static boolean IsSumSubarray(int arr[],int n,int sum)
+	{
+		HashSet<Integer>s=new HashSet<Integer>();
+		int PreSum=0;
+		for(int i=0;i<n;i++)
+		{
+			PreSum=PreSum+arr[i];
+			if(PreSum==sum)
+			{
+				return true;
+			}
+			if(s.contains(PreSum-sum))
+			{
+				return true;
+			}
+
+			s.add(PreSum);
+		}
+		return false;
+	}
+}
+// find subArray if given sum is present or not in subArray
