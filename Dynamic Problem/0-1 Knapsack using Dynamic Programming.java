210e443546eb8660123ba39245e8ae1a4fa3aaba
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-06-27 16:18:16 +0530

    Create 0-1 Knapsack using Dynamic Programming.java

---
+++ Dynamic Problem/0-1 Knapsack using Dynamic Programming.java
@@ -0,0 +1,30 @@
+static int knshp(int W,int wt[],int val[],int n){
+        int dp[][]=new int[n+1][W+1];
+        for(int i=0;i<=W;i++){
+            dp[0][i]=0;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=W;j++){
+                if(wt[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=Math.max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n][W];
+    }
+    
+    
+    /*
+    here W= maximum capacity of knapsack and n=number of weight & value, wt= weight of and val is corresponding to value
+    .
+    ip= val[]={10,40,30,50}
+        wt[]={5,4,6,3}
+        W=10
+    op= 90
+    */
